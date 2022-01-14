13a6a375d29c58e866d255152b94a49341729c4f
DouglasHdezT <00004516@uca.edu.sv>
2022-01-14 00:57:21 -0600

    :sparkles: Add: Excercise 1

--- Exercise01.cpp
+++ Exercise01.cpp
@@ -3,5 +3,31 @@
 using namespace std;
 
 int main(void) {
-  cout << "Prueba";
+  int arrayLength = 5;
+  char charArr[] = {'a', 'b', 'c', 'd', 'e'};
+  char *pointer;
+
+  pointer = charArr;
+
+  cout << "NORMAL" << endl;
+  for (int i = 0; i < arrayLength; i++){
+    cout << "Elemento " << i+1 << ": " << *(pointer + i) << endl;
+  }
+
+  cout << endl;
+
+  cout << "REVERSE" << endl;
+  for (int i = (arrayLength - 1); i >= 0; i--){
+    cout << "Elemento " << i+1 << ": " << *(pointer + i) << endl;
+  }
+
+  cout << endl;
+
+  cout << "REVERSE ALT" << endl;
+  pointer = charArr + (arrayLength - 1);
+  for (int i = 0; i < arrayLength; i++){
+    cout << "Elemento " << arrayLength - i << ": " << *(pointer - i) << endl;
+  }
+  
+  return 0;
 }
